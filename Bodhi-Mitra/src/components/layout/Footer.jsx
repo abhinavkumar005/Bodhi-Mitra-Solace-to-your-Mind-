6b2c757a66dc8f71afb5d67cab25480a6f6f30c6
abhi-create-design <abhi.prajapati2005@gmail.com>
2026-01-12 17:32:20 +0530

    Added Bodhi-Mitra as normal folder

---
+++ Bodhi-Mitra/src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+// src/components/layout/Footer.jsx
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-4 text-center">
+      © {new Date().getFullYear()} Managed & Developed by Abhishek Prajapati, Abhinav Kumar, and Vivek Khatkar
+    </footer>
+  );
+}
